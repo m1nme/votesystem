4c65dfd95fdb6febff8d0f4090e80bbd2cb296d1
Chathurque <huhtgertow@126.com>
2020-11-25 11:13:41 +0800

    帖子子

--- manager/urls.py
+++ manager/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import login,isLogin
 from . import showCats,getCatInfo,modifyCatInfo
+from . import showPosts,getPostInfo,modifyPostInfo
 urlpatterns = [
     path('login/', login.login),
     path('isLogin/', isLogin.isLogin),
@@ -9,4 +10,8 @@ urlpatterns = [
     path('showCats/', showCats.showCats),
     path('getCatInfo/', getCatInfo.getCatInfo),
     path('modifyCatInfo/', modifyCatInfo.modifyCatInfo),
+
+    path('showPosts/', showPosts.showPosts),
+    path('getPostInfo/', getPostInfo.getPostInfo),
+    path('modifyPostInfo/', modifyPostInfo.modifyPostInfo),
 ]
